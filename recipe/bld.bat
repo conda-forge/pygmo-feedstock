f35e50678407c7aa083b369e3d4537342344fe7e
Francesco Biscani <bluescarni@gmail.com>
2019-06-24 07:04:00 +0200

    Try disabling Boost_CMAKE on Windows too.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,7 @@ cd build
 
 cmake ^
     -G "NMake Makefiles" ^
+    -DBoost_NO_BOOST_CMAKE=ON ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DPAGMO_BUILD_PAGMO=no ^
