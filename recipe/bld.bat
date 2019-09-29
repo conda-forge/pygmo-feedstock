c0980dfb730e69f3f95eebd17b0f02e1b7547384
Francesco Biscani <bluescarni@gmail.com>
2019-09-29 22:16:09 +0200

    Explicitly set the build type to Release on Windows.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,7 @@ cd build
 
 cmake ^
     -G "NMake Makefiles" ^
+    -DCMAKE_BUILD_TYPE=Release ^
     -DBoost_NO_BOOST_CMAKE=ON ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
@@ -10,4 +11,4 @@ cmake ^
     -DPAGMO_BUILD_PYGMO=yes ^
     ..
 
-cmake --build . --config Release --target install
+cmake --build . --target install
