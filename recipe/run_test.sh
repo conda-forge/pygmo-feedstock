e8c018e7f70ac8800798c33fd9b632836982be2c
Francesco Biscani <bluescarni@gmail.com>
2019-12-06 01:26:04 +0100

    Try disabling ipyparallel on py3.8.

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-ipcluster start --daemonize=True;
+# ipcluster start --daemonize=True;
 
 # Give some time for the cluster to start up.
-sleep 20;
+# sleep 20;
 
 # Run the test suite
 python -c "import pygmo; pygmo.test.run_test_suite()"
