8b250198a87e1e656293a8d598fd777a5372ed8c
Francesco Biscani <bluescarni@gmail.com>
2020-03-04 23:18:33 +0100

    Compile in C++14 mode in OSX.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,9 +3,15 @@
 mkdir build
 cd build
 
+if [[ "$(uname)" == "Darwin" ]]; then
+    export CPP_STANDARD=14
+else
+    export CPP_STANDARD=17
+fi
+
 cmake \
     -DBoost_NO_BOOST_CMAKE=ON \
-    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_CXX_STANDARD=$CPP_STANDARD \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
