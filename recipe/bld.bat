ded968c4e0808bf002c625647e0728e42cb6e10c
Francesco Biscani <bluescarni@gmail.com>
2020-12-22 19:51:48 +0100

    Remove the pybind11 pinning, depend on the pybind11-abi package instead.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,28 +1,11 @@
 mkdir build
 cd build
-SET PYGMO_BUILD_DIR=%cd%
-
-git clone https://github.com/pybind/pybind11.git
-cd pybind11
-git checkout 4f72ef846fe8453596230ac285eeaa0ce3278bb4
-mkdir build
-cd build
-cmake ^
-    -G "Ninja" ^
-    -DPYBIND11_TEST=NO ^
-    -DCMAKE_INSTALL_PREFIX=%PYGMO_BUILD_DIR% ^
-    -DCMAKE_PREFIX_PATH=%PYGMO_BUILD_DIR% ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    ..
-cmake --build . --target install
-cd ../..
 
 cmake ^
     -G "Visual Studio 15 2017 Win64" ^
     -DBoost_NO_BOOST_CMAKE=ON ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -Dpybind11_DIR=%PYGMO_BUILD_DIR%\share\cmake\pybind11\ ^
     -DPYGMO_ENABLE_IPO=yes ^
     ..
 
--- recipe/build.sh
+++ recipe/build.sh
@@ -12,28 +12,11 @@ else
     export RANLIB_CMAKE_SETTING="-DCMAKE_CXX_COMPILER_RANLIB=$GCC_RANLIB -DCMAKE_C_COMPILER_RANLIB=$GCC_RANLIB"
 fi
 
-export PYGMO_BUILD_DIR=`pwd`
-
-git clone https://github.com/pybind/pybind11.git
-cd pybind11
-git checkout 4f72ef846fe8453596230ac285eeaa0ce3278bb4
-mkdir build
-cd build
-pwd
-cmake \
-    -DPYBIND11_TEST=NO \
-    -DCMAKE_INSTALL_PREFIX=$PYGMO_BUILD_DIR \
-    -DCMAKE_PREFIX_PATH=$PYGMO_BUILD_DIR \
-    ..
-make install
-cd ../..
-
 cmake \
     -DBoost_NO_BOOST_CMAKE=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
-    -Dpybind11_DIR=$PYGMO_BUILD_DIR/share/cmake/pybind11/ \
     -DPYGMO_ENABLE_IPO=yes \
     $AR_CMAKE_SETTING \
     $RANLIB_CMAKE_SETTING \
