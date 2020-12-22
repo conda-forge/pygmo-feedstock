c5dea052d725d727b34e29aa486072cc1af0a0e2
Francesco Biscani <bluescarni@gmail.com>
2020-12-22 20:35:20 +0100

    Try removing the py38 special casing in the script.

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -3,11 +3,9 @@
 # Fetch the Python version.
 PYTHON_VER=`python -c "import sys; print('{}.{}'.format(sys.version_info.major,sys.version_info.minor))"`
 
-if [[ "${PYTHON_VER}" != "3.8" ]]; then
-    ipcluster start --daemonize=True;
-    # Give some time for the cluster to start up.
-    sleep 20;
-fi
+ipcluster start --daemonize=True;
+# Give some time for the cluster to start up.
+sleep 20;
 
 # Run the test suite
 python -c "import pygmo; pygmo.test.run_test_suite(); pygmo.mp_island.shutdown_pool(); pygmo.mp_bfe.shutdown_pool()"
