cd0cb73952765465d05a07e190f804d04c868594
Francesco Biscani <bluescarni@gmail.com>
2021-02-16 11:12:19 +0100

    Simplify/improve the build script on unix archs.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,25 +3,14 @@
 mkdir build
 cd build
 
-if [[ "$(uname)" == "Darwin" ]]; then
-    export AR_CMAKE_SETTING=
-    export RANLIB_CMAKE_SETTING=
-else
-    # Workaround for making the LTO machinery work on Linux.
-    export AR_CMAKE_SETTING="-DCMAKE_CXX_COMPILER_AR=$GCC_AR -DCMAKE_C_COMPILER_AR=$GCC_AR"
-    export RANLIB_CMAKE_SETTING="-DCMAKE_CXX_COMPILER_RANLIB=$GCC_RANLIB -DCMAKE_C_COMPILER_RANLIB=$GCC_RANLIB"
-fi
-
-cmake \
+cmake ${CMAKE_ARGS} \
     -DBoost_NO_BOOST_CMAKE=ON \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DPYGMO_ENABLE_IPO=yes \
-    $AR_CMAKE_SETTING \
-    $RANLIB_CMAKE_SETTING \
     ..
 
-make
+make -j${CPU_COUNT} VERBOSE=1
 
 make install
