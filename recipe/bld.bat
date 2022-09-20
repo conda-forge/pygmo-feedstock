1d26401be876881b409b981235d3a9c7bd7b7caa
Francesco Biscani <bluescarni@gmail.com>
2022-09-20 21:02:04 +0200

    Try changing the generator on Windows.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,7 @@ mkdir build
 cd build
 
 cmake ^
-    -G "Visual Studio 15 2017 Win64" ^
+    -G "Visual Studio 16 2019" -A x64 ^
     -DPython3_EXECUTABLE=%PREFIX%\python.exe ^
     -DBoost_NO_BOOST_CMAKE=ON ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
